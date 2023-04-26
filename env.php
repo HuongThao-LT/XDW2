3211b6b211e04817f77d5970f2f3f78634b4a2a5
DinhNPVy <79675139+DinhNPVy@users.noreply.github.com>
2023-04-26 10:02:58 +0700

    Add files via upload

---
+++ env.php
@@ -0,0 +1,20 @@
+<?php
+
+// App
+define("APP_URL", "http://localhost:8081/CoffeeShop-Blue/");
+
+// MYSQL
+define("MYSQL_HOST", "localhost");
+define("MYSQL_USERNAME", "root");
+define("MYSQL_PORT", 3306);
+define("MYSQL_PASSWORD", "");
+define("MYSQL_DATABASE", "coffeeshop_v2");
+
+// Api
+define("API_URL", "http://127.0.0.1:8000/api/v1/");
+
+// access token
+define("ACCESS_TOKEN", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiZGVjNzZkN2I1NDU1YzI2YjQ2NGM1N2RlMjNhNjJmNWRjOGQ0Mjg3OGUyZDJiZjQ3NjQ1ZTRhNDRiYzg2N2IwM2I4YWVlNDEzNzdlMjQ1ZDAiLCJpYXQiOjE2ODA2MzQ4NDQuODI0OTUsIm5iZiI6MTY4MDYzNDg0NC44MjQ5NTMsImV4cCI6MTcxMjI1NzI0NC44MTk1MzgsInN1YiI6IjIiLCJzY29wZXMiOltdfQ.re6MDmjJzS1NvUNNwWjpzbFEw-pPpIcOpa7k3iyx-HCokv-G6gYfvwW74bWpnObq-oxjylKiXA-mrsCeLI8VpDUdcLHCsI5mCkrRkSS2_7iB5JuJ5hPEpFV_fjwEIGLNdSkeHrvzkFlnu3DnjA0qgkGWDk0UhVBwQl1225FY7zHk17Kdz7Bm95vb_ws_R78ZsmlIQK6iihE_SpeiOox6LiFElop98rXhXWiXxsQu-p98tBZzF3h7zH-v9-GYXXT99TGIk2garuOTh-LBFE9DCPwSgE_EDyCKisMonGvDkU7VDXmizr6ElX-vlNVrdam-DfhOQJtDSn-XAbcQdjIWdlZ_5J1_kXeY1iEPEdF-8kgr2U6MDU-lY0l8iKwdZhC6pK4ccKSA-QpZ8OiOhbUoTxG9T0y1t8Asw6EeZO5XrmRGj-tQe-SiSX0AZuvt2pSY9TcsBhHdcRy2_YomF0DbPhFLvj00vxNYPhJGFSLgEFoituCQQgDf8lgcRSj-8do3_exLswy6qRmC9LLz1XrpAYY6OdYgZfiVF6yP_o32MoVpRfUEJM8sa_QVIY1KPBwLq5uBLnwxDlDQlU72PClyGra94rY56VALoDg8cycQkEHgvThAJeUIE0wQYWYRM3D9PABZP2PrMnWH14CxjawABqTbwoE9zI5qjd8OWYOESrA");
+
+// coffee image path
+define("COFFEE_IMAGE_PATH", "/public/images/coffees/");
